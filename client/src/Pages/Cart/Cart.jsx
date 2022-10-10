07c1c461d1c51b34d1f6ecd5f734cc9193b2dba8
KManishY <my1000088@gmail.com>
2022-10-10 10:32:08 +0530

    fix

--- client/src/Pages/Cart/Cart.jsx
+++ client/src/Pages/Cart/Cart.jsx
@@ -1,127 +1,133 @@
-import { EditIcon } from "@chakra-ui/icons";
 import { Box, Button, Heading, IconButton, Text } from "@chakra-ui/react";
-import styled from "@emotion/styled";
 import style from "./cart.module.css";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  deleteCartData,
-  getCartData,
-  postCartDataAll,
-} from "../../Redux/AppReducer/action.js";
+import { deleteCartData, getCartData } from "../../Redux/AppReducer/action.js";
 
 const Cart = () => {
-  const dispatch = useDispatch();
-  const { cart } = useSelector((state) => state.getCartReducer);
-  console.log("cart: ", cart);
-  if (cart) {
-    const total_price = cart.reduce(
-      (sum, item) => sum + Number(item.prod_price),
-      0
-    );
-    console.log("price", total_price);
-  }
-  const [count, setCount] = useState(1);
-  const handelincres = () => {
-    setCount(count + 1);
-  };
-  const handeldec = () => {
-    if (count > 1) {
-      setCount(count - 1);
-    } else {
-      setCount(count);
-    }
-  };
+	const dispatch = useDispatch();
+	const { cart } = useSelector((state) => state.getCartReducer);
+	console.log("cart: ", cart);
+	if (cart) {
+		const total_price = cart.reduce(
+			(sum, item) => sum + Number(item.prod_price),
+			0
+		);
+		console.log("price", total_price);
+	}
+	const [count, setCount] = useState(1);
+	const handelincres = () => {
+		setCount(count + 1);
+	};
+	const handeldec = () => {
+		if (count > 1) {
+			setCount(count - 1);
+		} else {
+			setCount(count);
+		}
+	};
 
-  const handleDelete = (item) => {
-    const query = {
-      params: item,
-    };
-    dispatch(deleteCartData(query));
-    dispatch(getCartData());
-  };
+	const handleDelete = (item) => {
+		const query = {
+			params: item
+		};
+		dispatch(deleteCartData(query));
+		dispatch(getCartData());
+	};
 
-  useEffect(() => {
-    dispatch(getCartData());
-  }, []);
+	useEffect(() => {
+		dispatch(getCartData());
+	}, []);
 
-  const ReadMore = ({ children }) => {
-    const text = children;
-    const [isReadMore, setIsReadMore] = useState(true);
-    const toggleReadMore = () => {
-      setIsReadMore(!isReadMore);
-    };
-    return (
-      <p className="text">
-        {isReadMore ? text.slice(0, 100) : text}
-        <span onClick={toggleReadMore} className="read-or-hide">
-          {isReadMore ? "...read more" : " show less"}
-        </span>
-      </p>
-    )
-  };
-  //   console.log(cart);
+	const ReadMore = ({ children }) => {
+		const text = children;
+		const [isReadMore, setIsReadMore] = useState(true);
+		const toggleReadMore = () => {
+			setIsReadMore(!isReadMore);
+		};
+		return (
+			<p className='text'>
+				{isReadMore ? text.slice(0, 100) : text}
+				<span onClick={toggleReadMore} className='read-or-hide'>
+					{isReadMore ? "...read more" : " show less"}
+				</span>
+			</p>
+		);
+	};
+	//   console.log(cart);
 
-  return (
-    <Box m="auto" className={style.cart_div}>
-      {cart &&
-        cart.map((el) => (
-          <div key={el._id} className={style.main_div}>
-            <div>
-              <img
-                className={style.img}
-                src={el.prod_image}
-                alt={el.prod_name}
-              />
-            </div>
+	return (
+		<Box m='auto' className={style.cart_div}>
+			{cart &&
+				cart.map((el) => (
+					<div key={el._id} className={style.main_div}>
+						<div>
+							<img
+								className={style.img}
+								src={el.prod_image}
+								alt={el.prod_name}
+							/>
+						</div>
 
-            <div>
-              {/* product name */}
-              <div>
-                <Heading className={style.product_name}>{el.prod_name}</Heading>
-              </div>
-              {/* product discription */}
-              <div className="container">
-                <ReadMore className={style.prod_description}>
-                  {el.prod_desc}
-                </ReadMore>
+						<div>
+							{/* product name */}
+							<div>
+								<Heading className={style.product_name}>
+									{el.prod_name}
+								</Heading>
+							</div>
+							{/* product discription */}
+							<div className='container'>
+								<ReadMore className={style.prod_description}>
+									{el.prod_desc}
+								</ReadMore>
 
-                {/* <Text></Text> */}
-              </div>
+								{/* <Text></Text> */}
+							</div>
 
-              <div className={style.price_main_div}>
-                {/* price */}
+							<div className={style.price_main_div}>
+								{/* price */}
 
-                {/* discount */}
-                <div>
-                  <Text color={'teal'}> Price: &#x20b9;{el.prod_price*count}</Text>
-                  <Text>
-                    {" "}
-                    Discount: &#x20b9;
-                    <span className={style.discount}>{el.prod_discount}</span>
-                  </Text>
-                </div>
-                <div className={style.inc_dec_main_div}>
-                  <div style={{ display: "flex", gap: "10px" }}>
-                    <button onClick={handelincres}>+</button>
-                    <p>{count}</p>
-                    <button onClick={handeldec}>-</button>
-                  </div>
-                </div>
-              </div>
-              <Button colorScheme="teal"
-                className={style.delete_btn}
-                onClick={() => handleDelete(el.prod_id)}
-              >
-                delete
-              </Button>
-            </div>
-          </div>
-        ))}
+								{/* discount */}
+								<div>
+									<Text color={"teal"}>
+										{" "}
+										Price: &#x20b9;{el.prod_price * count}
+									</Text>
+									<Text>
+										{" "}
+										Discount: &#x20b9;
+										<span className={style.discount}>
+											{el.prod_discount}
+										</span>
+									</Text>
+								</div>
+								<div className={style.inc_dec_main_div}>
+									<div
+										style={{ display: "flex", gap: "10px" }}
+									>
+										<button onClick={handelincres}>
+											+
+										</button>
+										<p>{count}</p>
+										<button onClick={handeldec}>-</button>
+									</div>
+								</div>
+							</div>
+							<Button
+								colorScheme='teal'
+								className={style.delete_btn}
+								onClick={() => handleDelete(el.prod_id)}
+							>
+								delete
+							</Button>
+						</div>
+					</div>
+				))}
 
-      <Button className={style.order_btn}>Order Now</Button>
-    </Box>
-  );
-};
+			<Button className={style.order_btn}>Order Now</Button>
+		</Box>
+	);
+};;
 
 export default Cart;
