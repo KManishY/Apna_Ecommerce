598241b5a92f0c52e90f788ce6effc4ee8634ba8
ShwetaMishra0101 <93873372+ShwetaMishra0101@users.noreply.github.com>
2022-10-28 19:47:55 +0530

    home page

--- client/src/Pages/Homepage/Homepage.jsx
+++ client/src/Pages/Homepage/Homepage.jsx
@@ -5,7 +5,9 @@ const Homepage = () => {
 	const { data } = useSelector((state) => state.productReducer);
 	console.log("data: ", data);
 
-	return <div>this is Homepage</div>;
+	return <div>
+		<img style={{width:"100%",height:"95vh"}} src="https://i.pinimg.com/736x/8f/25/2c/8f252c8949460c94c0e3b4454ff1c55f.jpg" alt="home logo" />
+	</div>;
 };
 
 export default Homepage
