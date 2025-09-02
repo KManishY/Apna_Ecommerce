9f9cae52024ad039270276f1c75c330a2c525cd2
Manish Kumar <manish@corprowise.com>
2025-09-02 12:15:45 +0530

    api config chnages

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,4 +1,4 @@
 
 
-// export const baseURL = "https://homedecorserver.onrender.com";
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://homedecorserver.onrender.com";
+// export const baseURL = "http://localhost:8080";
