b3db97fa03a6db9de0ec4e15722735132fa01657
Manish Kumar <manish@corprowise.com>
2025-09-04 14:27:53 +0530

    api config changes

--- admin_dashboard/src/apiconfig.js
+++ admin_dashboard/src/apiconfig.js
@@ -1,2 +1,2 @@
-export const baseURL = "http://localhost:8080";
-// export const baseURL = "https://homedecorserver.onrender.com";
+// export const baseURL = "http://localhost:8080";
+export const baseURL = "https://homedecorserver.onrender.com";
